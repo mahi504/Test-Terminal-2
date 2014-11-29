3aaa3eb24879fc7f01450ae7f774ee7cc6a8f097
MAHESH <mahesh@MAHESHs-Mac-Pro.local>
2014-11-29 19:57:15 +0530

    added a test class

---
+++ Test Terminal 2.h
@@ -0,0 +1,13 @@
+//
+//  Test Terminal 2.h
+//  Test Terminal 2
+//
+//  Created by MAHESH on 29/11/14.
+//  Copyright (c) 2014 mahesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test_Terminal_2 : NSObject
+
+@end
---
+++ Test Terminal 2.m
@@ -0,0 +1,13 @@
+//
+//  Test Terminal 2.m
+//  Test Terminal 2
+//
+//  Created by MAHESH on 29/11/14.
+//  Copyright (c) 2014 mahesh. All rights reserved.
+//
+
+#import "Test Terminal 2.h"
+
+@implementation Test_Terminal_2
+
+@end
